c3f302f69c3621cc73b00a951fb51e90c2ec6066
Melissa Pignalosa <m.pignalosa@hotmail.com>
2023-11-26 12:35:03 +0800

    Created an array and used for..of loop

--- js/script.js
+++ js/script.js
@@ -39,3 +39,29 @@ clover.sleep();
 baxter.play();
 
 console.log(clover, baxter);
+
+//update properties and create an array of objects
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+//console.log(allPets);
+
+//display pets in browser
+const showPets = function (petArray) {
+    pets.innerHTML = "";
+    for (let pet of petArray) {
+        let status = "ready to play!";
+        if (pet.isTired >= 7){
+            status = "sleeping";
+        }
+        const li = document.createElement("li");
+        li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+        pets.append(li);
+    } 
+}
+
+//add click event
+statusButton.addEventListener("click", function(){
+    showPets(allPets);
+});
